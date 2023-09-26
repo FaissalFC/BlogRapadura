81d1f6ffc09ab386220069c5bac267eef8177a03
FaissalFC <142908515+FaissalFC@users.noreply.github.com>
2023-09-26 11:49:48 -0300

    Add files via upload

---
+++ Angular Dio class Blog/src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "AUDICAO PARA NOVA ATRIZ",
+    "description": "Coletivo Rapadura faz chamamento publico para selecionar nova Atriz para fazer o papel de mariera na peca Sertao Ariano",
+    "photoCover":"https://super.abril.com.br/wp-content/uploads/2016/10/super_imgde_onde_veio_a_expressao_bode_expiatorio.jpg?quality=90&strip=info&w=1024&resize=1200,800"
+  },
+  {
+    "id":"2",
+    "title": "Marcado para dezembro de 2023 a apresentacao da peca Sertao Ariano",
+    "description": "O coletivo Rapadura fecha pauta com o Centro Cultural de Brasilia e marca para dezembro de 2023 a apresentacao do espetaculo Sertao Ariano",
+    "photoCover":"https://jornaldaparaiba.com.br/wp-content/uploads/2022/06/ariano-suassuna.jpg"
+  }
+]
